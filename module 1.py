6ebe7f7a3dafe178659fe3029d43044f51cbebdf
Bhavin-hedau <bhedau658@gmail.com>
2024-09-13 19:22:14 +0530

    Add files via upload

---
+++ module 1.py
@@ -0,0 +1,22 @@
+moudule-1(SDLC)
+
+
+
+1)what is software?
+-software is a set of instruction, data or program used to 
+operate computers and execute specific task.
+
+2)what are the types of applications?
+- web application
+- mobile application
+- desktop application
+
+3)what is programing?
+- programming is a way to instruct the computer to perform 
+  various tasks.
+
+4)what is python?
+-python is a simple and rasy to understand language which 
+ feels like reading simple english.this preudo code nature 
+ of python makes it easy to learn and understandable by 
+ beginners.
